e08ba7e141401126b9a3e3e08cb10b31f5e76ec9
punyaumesh15 <77526853+punyaumesh15@users.noreply.github.com>
2021-01-16 15:45:23 +0530

    Update C.c

--- C Programming/C.c
+++ C Programming/C.c
@@ -1,11 +1,6 @@
 /*stdio.h is a library that operates input output functions of the program */
 #include<stdio.h>
 /*The program is written in the main function,this main does not return a value*/
-void main()
-{
-	// Using printf to send message to console out
-	printf("Hello World!");
-}
 /*The program is written in the main function,this main does not return a value*/
 int main()
 {
